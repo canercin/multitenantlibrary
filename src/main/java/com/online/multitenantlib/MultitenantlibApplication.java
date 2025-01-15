58b8fe86a1e3cdb84d5b5b208c09fcfcb5a99a1b
canercin <canercincin34@gmail.com>
2025-01-15 22:26:52 +0300

    project init commit

---
+++ src/main/java/com/online/multitenantlib/MultitenantlibApplication.java
@@ -0,0 +1,13 @@
+package com.online.multitenantlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitenantlibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultitenantlibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/online/multitenantlib/MultitenantlibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.multitenantlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitenantlibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
